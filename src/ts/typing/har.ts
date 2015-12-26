7662b3d44d0eec8c6a617693706f747c185c08e2
Michael Mrowetz <me@michaelmrowetz.com>
2015-12-26 22:43:22 +0900

    add full HAR 1.2 definitions

--- src/ts/typing/har.ts
+++ src/ts/typing/har.ts
@@ -1,89 +1,173 @@
+/**
+ * Based on the HAR 1.2 Spec
+ * http://www.softwareishard.com/blog/har-12-spec/
+ */
+
+//http://www.softwareishard.com/blog/har-12-spec/#log
+export interface Har {
+  version: string
+  creator: Creator
+  browser?: Browser
+  pages?: Array<Page>
+  entries: Array<Entry>
+  comment?: string
+  label?: string //TODO: Verify if this does exist
+}
+
+// http://www.softwareishard.com/blog/har-12-spec/#creator
 export interface Creator {
   name: string
   version: string
 }
 
-export interface NameValueObj {
+// http://www.softwareishard.com/blog/har-12-spec/#browser
+export interface Browser {
   name: string
-  value: string
+  version: string
+  comment?: string
 }
 
-export interface Cookie {
-  expires: Date
-  httpOnly: boolean
-  name: string
-  secure: boolean
-  value: string
+//http://www.softwareishard.com/blog/har-12-spec/#pages
+export interface Page {
+  startedDateTime: string //YYYY-MM-DDThh:mm:ss.sTZD
+  id: string
+  title: string
+  pageTimings: PageTimings
+  comment?: string
 }
 
-export interface Request {
-  bodySize: number
-  cookies: Array<Cookie>
-  headers: Array<NameValueObj>
-  headersSize: number
-  httpVersion: string
-  method: string
-  queryString: Array<NameValueObj>
-  url: string
+//http://www.softwareishard.com/blog/har-12-spec/#pageTimings
+export interface PageTimings {
+  onContentLoad?: number
+  onLoad?: number
+  comment?: string
 }
 
-export interface Content {
-  encoding: string
-  mimeType: string
-  size: number
-  text: string
-}
-
-export interface Response {
-  _transferSize: number
-  bodySize: number
-  content: Content
-  cookies: Array<Cookie>
-  headers: Array<NameValueObj>
-  headersSize: number
-  httpVersion: string
-  redirectURL: string
-  status: number
-  statusText: string
-}
-
-export interface Timings {
-  blocked: number
-  connect: number
-  dns: number
-  receive: number
-  send: number
-  ssl: number
-  wait: number
-}
-
+//http://www.softwareishard.com/blog/har-12-spec/#entries
 export interface Entry {
-  cache: Object
-  connection: string
-  pageref: string
+  pageref?: string
+  startedDateTime: string //YYYY-MM-DDThh:mm:ss.sTZD
+  time: number
   request: Request
   response: Response
-  startedDateTime: string
-  time: number
+  cache: Cache
   timings: Timings
+  serverIPAddress?: string
+  connection?: string
+  comment?: string
 }
 
-export interface PageTimings {
-  onContentLoad: number
-  onLoad: number
+//http://www.softwareishard.com/blog/har-12-spec/#request
+export interface Request {
+  method: string
+  url: string
+  httpVersion: string
+  cookies: Array<Cookie>
+  headers: Array<Header>
+  queryString: Array<QueryString>
+  postData?: PostData
+  headersSize: number
+  bodySize: number
+  comment?: string
 }
 
-export interface Page {
-  id: string
-  pageTimings: PageTimings
-  startedDateTime: string
-  title: string
+//http://www.softwareishard.com/blog/har-12-spec/#response
+export interface Response {
+  status: number
+  statusText: string
+  httpVersion: string
+  cookies: Array<Cookie>
+  headers: Array<Header>
+  content: Content
+  redirectURL: string
+  headersSize: number
+  bodySize: number
+  comment?: string
+  _transferSize?: number
 }
 
-export interface Har {
-  label: string
-  creator: Creator
-  entries: Array<Entry>
-  pages: Array<Page>
-  version: string
+//http://www.softwareishard.com/blog/har-12-spec/#cookies
+export interface Cookie {
+  name: string
+  value: string
+  path?: string
+  domain?: string
+  expires?: Date
+  httpOnly?: boolean
+  secure?: boolean
+  comment?: string
+}
+
+//custom helper type
+interface NameValuePair {
+  name: string
+  value: string
+  comment?: string
+}
+
+//http://www.softwareishard.com/blog/har-12-spec/#headers
+export interface Header extends NameValuePair { }
+
+//http://www.softwareishard.com/blog/har-12-spec/#queryString
+export interface QueryString extends NameValuePair { }
+
+//http://www.softwareishard.com/blog/har-12-spec/#postData
+export interface PostData {
+  mimeType: string
+  params: Array<Param>
+  text: string
+  comment?: string
+}
+
+//http://www.softwareishard.com/blog/har-12-spec/#params
+export interface Param {
+  name: string
+  value?: string
+  fileName?: string
+  contentType?: string
+  comment?: string
+}
+
+//http://www.softwareishard.com/blog/har-12-spec/#content
+export interface Content {
+  size: number
+  compression?: number
+  mimeType: string
+  text?: string
+  encoding?: string
+  comment?: string
+}
+
+//http://www.softwareishard.com/blog/har-12-spec/#cache
+export interface Cache {
+  beforeRequest?: CacheDetails
+  afterRequest?: CacheDetails
+  comment ?: string
+}
+
+export interface CacheDetails{
+  expires?: string
+  lastAccess: string
+  eTag: string
+  hitCount: number
+  comment?: string
+}
+
+//http://www.softwareishard.com/blog/har-12-spec/#timings
+export interface Timings {
+  /**  Time spent in a queue waiting for a network connection. Use -1 if the timing does not apply to the current request. */
+  blocked?: number
+  /**  DNS resolution time. The time required to resolve a host name. Use -1 if the timing does not apply to the current request. */
+  dns?: number
+  /** Time required to create TCP connection. Use -1 if the timing does not apply to the current request. */
+  connect?: number
+  /** Time required to send HTTP request to the server. */
+  send?: number
+  /** Waiting for a response from the server.] */
+  wait: number
+  /** Time required to read entire response from the server (or cache). */
+  receive: number
+  /** Time required for SSL/TLS negotiation. If this field is defined then the time is also included in the connect field (to ensure backward compatibility with HAR 1.1). Use -1 if the timing does not apply to the current request. */
+  ssl?: number
+  comment?: string
 }
