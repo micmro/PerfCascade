28be34a528ae9b33cb2cde07938b47d3daf5d03b
Michael Mrowetz <me@michaelmrowetz.com>
2016-10-16 17:43:36 +0900

    Release v0.2.1

--- src/dist/perf-cascade.js
+++ src/dist/perf-cascade.js
@@ -1,4 +1,4 @@
-/*! github.com/micmro/PerfCascade Version:0.1.6 (02/07/2016) */
+/*! github.com/micmro/PerfCascade Version:0.2.1 (16/10/2016) */
 
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.perfCascade = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 /**
@@ -756,6 +756,9 @@ exports.default = HarTransformer;
  * @param {string} mimeType
  */
 function mimeToRequestType(mimeType) {
+    if (mimeType === undefined) {
+        return "other";
+    }
     var types = mimeType.split("/");
     var part2 = types[1];
     // take care of text/css; charset=UTF-8 etc
@@ -1667,7 +1670,7 @@ function createTimeScale(durationMs, diagramHeight) {
     var timeHolder = svg.newEl("g", { class: "time-scale full-width" });
     for (var i = 0, secs = durationMs / 1000, secPerc = 100 / secs; i <= secs; i++) {
         (function (i, secs, secPerc) {
-            var lineLabel = svg.newTextEl(i + "sec", diagramHeight);
+            var lineLabel = svg.newTextEl(i + "s", diagramHeight);
             if (i > secs - 0.2) {
                 lineLabel.setAttribute("x", secPerc * i - 0.5 + "%");
                 lineLabel.setAttribute("text-anchor", "end");
