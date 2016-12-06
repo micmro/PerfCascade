093f4f70b7684ca0379be9c214f258488b4c6c70
Michael Mrowetz <me@michaelmrowetz.com>
2016-12-06 21:40:20 +0900

    #68 swap connect and SSL (follow network order) and add HAR Spec expanation.

--- src/ts/legend/legend.ts
+++ src/ts/legend/legend.ts
@@ -6,12 +6,13 @@ export function makeLegend(): HTMLUListElement {
   ulNode.className = "resource-legend"
 
   ulNode.innerHTML = `
-        <li class="legend-blocked">Blocked</li>
-        <li class="legend-dns">DNS</li>
-        <li class="legend-ssl">SSL (TLS)</li>
-        <li class="legend-connect">Connect</li>
-        <li class="legend-send">Send</li>
-        <li class="legend-wait">Wait</li>
-        <li class="legend-receive">Receive</li>`
+        <li class="legend-blocked" title="Time spent in a queue waiting for a network connection.">Blocked</li>
+        <li class="legend-dns" title="DNS resolution time.">DNS</li>
+        <li class="legend-connect" title="Time required to create TCP connection.">Connect</li>
+        <li class="legend-ssl" title="Time required for SSL/TLS negotiation.
+Time is also included in the connect field (to ensure backward compatibility with HAR 1.1).">SSL (TLS)</li>
+        <li class="legend-send" title="Time required to send HTTP request to the server.">Send</li>
+        <li class="legend-wait" title="Waiting for a response from the server.">Wait</li>
+        <li class="legend-receive" title="Time required to read entire response from the server (or cache).">Receive</li>`
   return ulNode
 }
