ced9161bf908e093d647de002eb01e906555027f
Tobias Lidskog <tobias@lidskog.se>
2016-12-22 21:52:10 +0100

    Move har parse helper to har.ts.
    This makes styling-converters.ts only have styling (css) related functions.

--- src/ts/transformers/har.ts
+++ src/ts/transformers/har.ts
@@ -7,9 +7,6 @@ import {
   WaterfallData,
   Mark, RequestType, TimingType
 } from "../typing/waterfall"
-import {
-  mimeToRequestType
-} from "./styling-converters"
 
 class WaterfallEntry {
   public total: number
@@ -30,6 +27,47 @@ class WaterfallEntryTiming {
               public end: number) {
     this.total = (typeof start !== "number" || typeof end !== "number") ? undefined : (end - start)
   }
+}
+
+/**
+ * Convert a MIME type into it's WPT style request type (font, script etc)
+ * @param {string} mimeType
+ */
+function mimeToRequestType(mimeType: string): RequestType {
+  if (mimeType === undefined) {
+    return "other"
+  }
+  let types = mimeType.split("/")
+  let part2 = types[1]
+  // take care of text/css; charset=UTF-8 etc
+  if (part2 !== undefined) {
+    part2 = part2.indexOf(";") > -1 ? part2.split(";")[0] : part2
+  }
+  switch (types[0]) {
+    case "image": return "image"
+    case "font": return "font"
+    case "video": return "video"
+    case "audio": return "audio"
+  }
+  switch (part2) {
+    case "svg+xml": return "svg"
+    case "xml":
+    case "html": return "html"
+    case "plain": return "plain"
+    case "css": return "css"
+    case "vnd.ms-fontobject":
+    case "font-woff":
+    case "font-woff2":
+    case "x-font-truetype":
+    case "x-font-opentype":
+    case "x-font-woff": return "font"
+    case "javascript":
+    case "x-javascript":
+    case "script":
+    case "json": return "javascript"
+    case "x-shockwave-flash": return "flash"
+  }
+  return "other"
 }
 
 export namespace HarTransformer {
--- src/ts/transformers/styling-converters.ts
+++ src/ts/transformers/styling-converters.ts
@@ -1,44 +1,4 @@
 import {RequestType, TimingType} from "../typing/waterfall";
-/**
- * Convert a MIME type into it's WPT style request type (font, script etc)
- * @param {string} mimeType
- */
-export function mimeToRequestType(mimeType: string): RequestType {
-  if (mimeType === undefined) {
-    return "other"
-  }
-  let types = mimeType.split("/")
-  let part2 = types[1]
-  // take care of text/css; charset=UTF-8 etc
-  if (part2 !== undefined) {
-    part2 = part2.indexOf(";") > -1 ? part2.split(";")[0] : part2
-  }
-  switch (types[0]) {
-    case "image": return "image"
-    case "font": return "font"
-    case "video": return "video"
-    case "audio": return "audio"
-  }
-  switch (part2) {
-    case "svg+xml": return "svg"
-    case "xml":
-    case "html": return "html"
-    case "plain": return "plain"
-    case "css": return "css"
-    case "vnd.ms-fontobject":
-    case "font-woff":
-    case "font-woff2":
-    case "x-font-truetype":
-    case "x-font-opentype":
-    case "x-font-woff": return "font"
-    case "javascript":
-    case "x-javascript":
-    case "script":
-    case "json": return "javascript"
-    case "x-shockwave-flash": return "flash"
-  }
-  return "other"
-}
 
 /**
  * Convert a RequestType into a CSS class
