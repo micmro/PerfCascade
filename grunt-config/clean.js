08d77ebe98f506a98a68359fdfa978ae3bd033f1
Michael Mrowetz <me@michaelmrowetz.com>
2017-01-14 22:39:07 +0900

    split gruntfile

---
+++ grunt-config/clean.js
@@ -0,0 +1,6 @@
+module.exports = {
+  dist: ["temp/", "src/dist/", "./index.js", "./index.d.ts"],
+  pages: ["gh-pages/"],
+  lib: ["lib/", "types/", "./index.js", "./index.d.ts"],
+  js: ["src/ts/**/*.js", "src/ts/**/*.js.map"]
+};
---
+++ grunt-config/gh-pages.js
@@ -0,0 +1,8 @@
+module.exports = {
+  options: {
+    base: "gh-pages",
+    add: true,
+    // tag: '<%= package.version %>'
+  },
+  src: ["**/*"]
+};
