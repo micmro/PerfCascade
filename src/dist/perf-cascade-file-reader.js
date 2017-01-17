14735f46e38237e62e05ae2e04bd04ac9ba75b22
Michael Mrowetz <me@michaelmrowetz.com>
2017-01-18 08:28:13 +0900

    Release v0.2.22

--- src/dist/perf-cascade-file-reader.js
+++ src/dist/perf-cascade-file-reader.js
@@ -1,4 +1,4 @@
-/*! github.com/micmro/PerfCascade Version:0.2.21 (16/01/2017) */
+/*! github.com/micmro/PerfCascade Version:0.2.22 (18/01/2017) */
 /*
  Copyright (c) 2013 Gildas Lormeau. All rights reserved.
 
@@ -3122,7 +3122,7 @@
 	env.Inflater = env._jzlib_Inflater = Inflater;
 })(this);
 
-/*! github.com/micmro/PerfCascade Version:0.2.20 (16/01/2017) */
+/*! github.com/micmro/PerfCascade Version:0.2.21 (18/01/2017) */
 
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.perfCascadeFileReader = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 "use strict";
