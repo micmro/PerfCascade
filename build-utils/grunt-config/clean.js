35529701b22d752e72be60036df5dd1a812cb85d
Michael Mrowetz <me@michaelmrowetz.com>
2017-01-29 20:59:09 +0900

    add task to clean old files and remove them from gitignore

--- build-utils/grunt-config/clean.js
+++ build-utils/grunt-config/clean.js
@@ -1,6 +1,8 @@
 module.exports = {
   all: ["build/"],
   dist: ["build/stage/"],
-  npm: ["build/npm/"],
+  //clean old files from before the build setup change
+  legacy: ["dist/", "types/", "src/dist/", "lib/", "stylesheets/", "gh-pages/", "index.d.ts", "index.js"],
+  //remove manual (incorrect) TS builds
   js: ["src/ts/**/*.js", "src/ts/**/*.js.map"]
 };
