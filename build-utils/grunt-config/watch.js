6abc41733455e0f535ffe901852b86127c292429
Michael Mrowetz <me@michaelmrowetz.com>
2017-02-06 23:04:18 +0900

    tooling: add seperate file-reader watch to avoid double-reload on dev-server and speed up re-build

--- build-utils/grunt-config/watch.js
+++ build-utils/grunt-config/watch.js
@@ -1,7 +1,15 @@
 module.exports = {
-  ts: {
-    files: ["src/ts/**/*.ts", "Gruntfile.js"],
-    tasks: ["distBase"],
+  ts: { // all ts except the file reader ones (to avoid double-reload)
+    files: ["src/ts/**/*.ts", "!src/ts/file-reader.ts"],
+    tasks: ["browserify:dist"],
+    options: {
+      spawn: false,
+      interrupt: true
+    },
+  },
+  fileReader: { //handle file-reader builds
+    files: ["src/ts/file-reader.ts", "src/zip/*.js"],
+    tasks: ["distFileReader"],
     options: {
       spawn: false,
       interrupt: true
