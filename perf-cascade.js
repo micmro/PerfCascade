0a8ae8be95b6ae35033c682b08a16a7eadab4daa
Michael Mrowetz <me@michaelmrowetz.com>
2017-02-22 23:23:48 +0900

    Release v0.6.1

--- perf-cascade.js
+++ perf-cascade.js
@@ -1,4 +1,4 @@
-/*! github.com/micmro/PerfCascade Version:0.6.0 (21/02/2017) */
+/*! github.com/micmro/PerfCascade Version:0.6.1 (22/02/2017) */
 
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.perfCascade = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 "use strict";
@@ -373,6 +373,15 @@ var htmlChars = new RegExp(Object.keys(htmlCharMap).join("|"), "g");
  * @param  {string} unsafe - string to be rendered in HTML
  */
 function escapeHtml(unsafe) {
+    if (unsafe === void 0) { unsafe = ""; }
+    if (typeof unsafe !== "string") {
+        if (typeof unsafe["toString"] === "function") {
+            unsafe = unsafe.toString();
+        }
+        else {
+            throw TypeError("Invalid parameter");
+        }
+    }
     return unsafe.replace(htmlChars, function (match) {
         return htmlCharMap[match];
     });
