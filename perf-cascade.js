57cb64177145d52efc7ec1c98db375ee32b10550
Michael Mrowetz <me@michaelmrowetz.com>
2017-03-02 22:25:56 +0900

    Release v0.8.0

--- perf-cascade.js
+++ perf-cascade.js
@@ -1,4 +1,4 @@
-/*! github.com/micmro/PerfCascade Version:0.7.0 (01/03/2017) */
+/*! github.com/micmro/PerfCascade Version:0.8.0 (02/03/2017) */
 
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.perfCascade = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 "use strict";
@@ -2211,11 +2211,10 @@ var appendSecond = function (context, timeHolder, secsTotal, sec, addLabel) {
  * Renders the time-scale SVG elements (1sec, 2sec...)
  * @param  {Context} context  Execution context object
  * @param {number} durationMs    Full duration of the waterfall
- * @param {number} subSecondStepMs  Distant (time in ms) between sub-second time-scales
  */
-function createTimeScale(context, durationMs, subSecondStepMs) {
-    if (subSecondStepMs === void 0) { subSecondStepMs = 200; }
+function createTimeScale(context, durationMs) {
     var timeHolder = svg.newG("time-scale full-width");
+    var subSecondStepMs = Math.ceil(durationMs / 10000) * 200;
     /** steps between each second marker */
     var subSecondSteps = 1000 / subSecondStepMs;
     var secs = durationMs / 1000;
