7ca246a979bbb7643a0fb0f9c63a0aea068fe552
Michael Mrowetz <me@michaelmrowetz.com>
2017-03-25 17:18:46 +0900

    Release v1.2.0

--- perf-cascade.js
+++ perf-cascade.js
@@ -1,4 +1,4 @@
-/*! github.com/micmro/PerfCascade Version:1.1.0 (24/03/2017) */
+/*! github.com/micmro/PerfCascade Version:1.2.0 (25/03/2017) */
 
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.perfCascade = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 "use strict";
@@ -2539,10 +2539,12 @@ function createWaterfallSvg(data, options) {
     var maxNumberWidth = svg.getNodeTextWidth(svg.newTextEl("" + widestRequestNumber), true);
     var barEls = [];
     function getChartHeight() {
-        return (chartHolderHeight + context.overlayManager.getCombinedOverlayHeight()).toString() + "px";
+        return chartHolderHeight + context.overlayManager.getCombinedOverlayHeight();
     }
     context.pubSub.subscribeToOverlayChanges(function () {
-        timeLineHolder.style.height = getChartHeight();
+        var newHeight = getChartHeight();
+        timeLineHolder.classList.toggle("closing", newHeight < timeLineHolder.clientHeight);
+        timeLineHolder.style.height = newHeight + "px";
     });
     /** Renders single row and hooks up behaviour */
     function renderRow(entry, i) {
