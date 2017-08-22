2e44defe3babeb73a1dfdc40fc542066e91d83d7
soulgalore <peter@soulgalore.com>
2017-08-22 07:56:18 +0200

    Set row max time from outside of PerfCascade
    The use case for this is when you wanna put to waterfall charts
    on top on each other and you wanna make sure they have the
    same time line, so it is easier to compare them.

--- src/ts/typing/options.ts
+++ src/ts/typing/options.ts
@@ -19,6 +19,8 @@ export interface ChartRenderOption {
   legendHolder: HTMLElement;
   /** Callback called when the HAR doc has been parsed into PerfCascases */
   onParsed: (data: WaterfallDocs) => void;
+  /** Set a row max time in ms (if not set the time is calculated from the HAR)  */
+  rowMaxTimeInMs: number;
 }
 
 export interface HarTransformerOptions {
