22cc3dba2297864392fafb8d74b9e1575ad20734
Peter Hedenskog <phedenskog@wikimedia.org>
2018-02-13 13:13:25 +0100

    Add null check for content.
    There are HAR files from WebPageTest that failed, see #217.

--- src/ts/helpers/parse.ts
+++ src/ts/helpers/parse.ts
@@ -125,7 +125,11 @@ const htmlChars = new RegExp(Object.keys(htmlCharMap).join("|"), "g");
  * @param  {string} unsafe - string to be rendered in HTML
  */
 export function escapeHtml(unsafe: string | number | boolean = ""): string {
-  if (typeof unsafe !== "string") {
+  // See https://github.com/micmro/PerfCascade/issues/217
+  if (unsafe == null) {
+    return "";
+  }
+  else if (typeof unsafe !== "string") {
     if (typeof unsafe["toString"] === "function") {
       unsafe = unsafe.toString();
     } else {
