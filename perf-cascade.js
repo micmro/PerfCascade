645caba05c36e7f61ea3ed16b120cfe1ce8463d4
Michael <michaelmrowetz@gmail.com>
2018-05-27 23:12:30 +0200

    Release v2.5.1

--- perf-cascade.js
+++ perf-cascade.js
@@ -1,4 +1,4 @@
-/*! github.com/micmro/PerfCascade Version:2.5.0 (09/04/2018) */
+/*! github.com/micmro/PerfCascade Version:2.5.1 (27/05/2018) */
 
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.perfCascade = f()}})(function(){var define,module,exports;return (function(){function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s}return e})()({1:[function(require,module,exports){
 "use strict";
@@ -2191,16 +2191,17 @@ function createRowInfoOverlay(overlay, y, detailsHeight) {
     var closeBtn = createCloseButtonSvg(y);
     closeBtn.addEventListener("click", function () { return overlay.onClose(overlay.index); });
     var body = html_details_body_1.createDetailsBody(requestID, detailsHeight, overlay.entry);
-    var buttons = body.getElementsByClassName("tab-button");
-    var tabs = body.getElementsByClassName("tab");
+    // need to re-fetch the elements to fix Edge "Invalid Calling Object" bug
+    var getButtons = function () { return body.getElementsByClassName("tab-button"); };
+    var getTabs = function () { return body.getElementsByClassName("tab"); };
     var setTabStatus = function (tabIndex) {
         overlay.openTabIndex = tabIndex;
-        dom_1.forEachNodeList(tabs, function (tab, j) {
+        dom_1.forEachNodeList(getTabs(), function (tab, j) {
             tab.style.display = (tabIndex === j) ? "block" : "none";
-            buttons.item(j).classList.toggle("active", (tabIndex === j));
+            getButtons().item(j).classList.toggle("active", (tabIndex === j));
         });
     };
-    dom_1.forEachNodeList(buttons, function (btn, tabIndex) {
+    dom_1.forEachNodeList(getButtons(), function (btn, tabIndex) {
         btn.addEventListener("click", function () { return setTabStatus(tabIndex); });
     });
     setTabStatus(overlay.openTabIndex);
