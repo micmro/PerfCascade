5a3d5af49f8721e2dffa04c4a1d9e288229b0674
Michael <michaelmrowetz@gmail.com>
2019-04-08 21:37:09 +0200

    Release v2.5.7

--- perf-cascade.js
+++ perf-cascade.js
@@ -1,4 +1,4 @@
-/*! github.com/micmro/PerfCascade Version:2.5.6 (17/11/2018) */
+/*! github.com/micmro/PerfCascade Version:2.5.7 (08/04/2019) */
 
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.perfCascade = f()}})(function(){var define,module,exports;return (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 "use strict";
@@ -1539,7 +1539,13 @@ function transformPage(harData, pageIndex, options) {
     })
         .map(function (entry, index) {
         var startRelative = new Date(entry.startedDateTime).getTime() - pageStartTime;
-        doneTime = Math.max(doneTime, startRelative + entry.time);
+        if (!isNaN(startRelative)) {
+            doneTime = Math.max(doneTime, startRelative + entry.time);
+        }
+        else {
+            // tslint:disable-next-line:no-console
+            console.warn("Entry has no valid 'startedDateTime' time", entry.request.url, entry);
+        }
         return toWaterFallEntry(entry, index, startRelative, isTLS);
     });
     var marks = getMarks(pageTimings, currPage, options);
