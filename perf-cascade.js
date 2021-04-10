cade7227a8de6a1c0f0e6f7f716241ac12c80a4e
Michael <michaelmrowetz@gmail.com>
2021-04-10 11:45:38 -0400

    Release v2.8.1

--- perf-cascade.js
+++ perf-cascade.js
@@ -1,4 +1,4 @@
-/*! github.com/micmro/PerfCascade Version:2.8.0 (27/03/2021) */
+/*! github.com/micmro/PerfCascade Version:2.8.1 (10/04/2021) */
 
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.perfCascade = f()}})(function(){var define,module,exports;return (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 "use strict";
@@ -536,7 +536,7 @@ function parseAndFormat(input, parseFn, formatFn) {
 }
 exports.parseAndFormat = parseAndFormat;
 function toString(source) {
-    if ("toString" in source && typeof source === "function") {
+    if (source["toString"] && typeof source["toString"] === "function") {
         return source.toString();
     }
     else {
