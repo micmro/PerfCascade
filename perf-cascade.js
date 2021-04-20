875aff082f637676e762837f2382d5124b7adbdc
Michael <michaelmrowetz@gmail.com>
2021-04-19 20:42:50 -0400

    Release v2.9.0

--- perf-cascade.js
+++ perf-cascade.js
@@ -1,4 +1,4 @@
-/*! github.com/micmro/PerfCascade Version:2.8.1 (10/04/2021) */
+/*! github.com/micmro/PerfCascade Version:2.9.0 (19/04/2021) */
 
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.perfCascade = f()}})(function(){var define,module,exports;return (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 "use strict";
@@ -1864,6 +1864,10 @@ function makeRowCssClasses(status) {
         misc_1.isInStatusCodeRange(status, 300, 399)) {
         // 304 == Not Modified, so not an issue
         classes.push("status3xx");
+    }
+    else if (status === 0 || status === undefined) {
+        // eg connection refused, or connection timeout etc then the http status code defaults to 0
+        classes.push("status0");
     }
     return classes.join(" ");
 }
